d7f5412d6983844f9e5f635c6ecf08517fd5df89
TG-Brad <TG.Brad.TheLad@gmail.com>
2022-10-21 16:03:13 +1000

    Migrate state to hooks so it can be easily accessed

--- src/state/deploy/reducer.ts
+++ src/state/deploy/reducer.ts
@@ -1,7 +1,8 @@
 import { createReducer } from '@reduxjs/toolkit'
-import { Field, replaceDeployState,  } from './actions'
+import { Field, replaceDeployState, ownerShareChange, daysToLockChange, toggleLockForever, paramsChange, templateChange } from './actions'
 import { selectCurrency, setRecipient, switchCurrencies, typeInput } from '../swap/actions'
 import { Currency } from 'custom_modules/@filterswap-libs/sdk/dist/sdk.esm'
+import { deployTokenTemplates } from '../../constants/deployToken/templates'
 
 export interface DeployState {
   readonly independentField: Field
@@ -12,7 +13,24 @@ export interface DeployState {
   readonly [Field.OUTPUT]: {
     readonly currencyId: string | undefined
   }
-  readonly params: object
+  readonly params: any
+  readonly ownerShare: number
+  readonly liquidityShare: number
+  readonly daysToLock: number
+  readonly lockForever: boolean
+  readonly selectedTemplate: number
+  readonly createOptions: {
+    description: string
+    name: string
+    options: {
+      fieldName: string
+      id: string
+      type?: string
+      value?: number
+      min?: number
+      max?: number
+    }[]
+  }
 }
 
 const initialState: DeployState = {
@@ -25,15 +43,37 @@ const initialState: DeployState = {
     currencyId: '',
   },
   params: {
-    
-  }
+    tokenName: '',
+    tokenSymbol: '',
+    totalSupply: '1000000',
+    transferFee: '',
+    buyFee: '',
+    sellFee: '',
+  },
+  ownerShare: 0,
+  liquidityShare: 100,
+  daysToLock: 14,
+  lockForever: false,
+  selectedTemplate: 0,
+  createOptions: deployTokenTemplates[0]
 }
 
 export default createReducer<DeployState>(initialState, (builder) =>
   builder
     .addCase(
       replaceDeployState,
-      (state, { payload: { typedValue, field, inputCurrencyId, params } }) => {
+      (state, { payload: {
+        typedValue,
+        field,
+        inputCurrencyId,
+        params,
+        ownerShare,
+        liquidityShare,
+        daysToLock,
+        lockForever,
+        selectedTemplate,
+        createOptions
+      } }) => {
         return {
           [Field.INPUT]: {
             currencyId: inputCurrencyId,
@@ -43,13 +83,17 @@ export default createReducer<DeployState>(initialState, (builder) =>
           },
           independentField: field,
           typedValue,
-          params
+          params,
+          ownerShare,
+          liquidityShare,
+          daysToLock,
+          lockForever,
+          selectedTemplate,
+          createOptions
         }
       }
     )
     .addCase(selectCurrency, (state, { payload: { currencyId, field } }) => {
-      const otherField = Field.INPUT
-      // the normal case
       return {
         ...state,
 				independentField: Field.INPUT,
@@ -61,6 +105,50 @@ export default createReducer<DeployState>(initialState, (builder) =>
         ...state,
         independentField: field,
         typedValue,
+      }
+    })
+    .addCase(ownerShareChange, (state, { payload: { ownerShare, liquidityShare } }) => {
+      return {
+        ...state,
+        ownerShare,
+        liquidityShare
+      }
+    })
+    .addCase(daysToLockChange, (state, { payload: { daysToLock } }) => {
+      return {
+        ...state,
+        daysToLock
+      }
+    })
+    .addCase(toggleLockForever, (state) => {
+      return {
+        ...state,
+        lockForever: !state.lockForever,
+      }
+    })
+    .addCase(paramsChange, (state, { payload: { id, value } }) => {
+      return {
+        ...state,
+        params: {
+          ...(state.params),
+          [id]: value
+        }
+      }
+    })
+    .addCase(templateChange, (state, { payload: { templateId } }) => {
+
+      console.log(templateId)
+      
+      const selectedTemplate = templateId
+      const createOptions = deployTokenTemplates[selectedTemplate]
+      console.log(deployTokenTemplates)
+      console.log(createOptions)
+      console.log(selectedTemplate)
+      
+      return {
+        ...state,
+        createOptions,
+        selectedTemplate
       }
     })
 )
