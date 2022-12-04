44c5680a704fed4cdc1b43f1dd9a0847b07ffd2c
BytePhoenixCoding <86841414+BytePhoenixCoding@users.noreply.github.com>
2022-12-04 23:30:22 +0000

    *TEMP FIX* fix later, 365 -> 14

--- src/state/mint/reducer.ts
+++ src/state/mint/reducer.ts
@@ -15,7 +15,7 @@ const initialState: MintState = {
   independentField: Field.CURRENCY_A,
   typedValue: '',
   otherTypedValue: '',
-  daysToLock: process.env.REACT_APP_LIQUIDITY_RECOMMENDED_LOCK_TIME,
+  daysToLock: 14,
   lockForever: false,
 }
 
